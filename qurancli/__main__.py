ccaf955c614be233c3234029d81eeeee5e9806c3
mehdi <mehdir26@gmail.com>
2019-04-18 13:34:34 -0400

    first commit

--- qurancli/__main__.py
+++ qurancli/__main__.py
@@ -14,24 +14,31 @@ def cli():
 @click.option('--surahnumber', help='Number of Surah', type=int, required=True)
 def read_surah(surahnumber):
     "Look up a Surah in the Quran"
-    r = requests.get('http://api.alquran.cloud/v1/surah/{}/en.ahmedraza'.format(surahnumber))
-    output = r.json()
+    try:
+        r = requests.get('http://api.alquran.cloud/v1/surah/{}/en.ahmedraza'.format(surahnumber))
+        output = r.json()
 
-    text = output['data']['ayahs']
+        text = output['data']['ayahs']
 
-    for i in text:
-        print(i['text'])
-        print('--')
+        for i in text:
+            print(i['text'])
+            print('--')
+    except:
+        print("Please enter a number between 1 and 114, as there are only 114 surahs in the Quran.")
+
 
 @cli.command('read-ayah')
 @click.option('--ayahnumber', help='Number of Ayah', type=int, required=True)
 def read_ayah(ayahnumber):
     "Look up an Ayah in the Quran"
-    r = requests.get('http://api.alquran.cloud/v1/ayah/{}/en.ahmedraza'.format(ayahnumber))
-    output = r.json()
+    try:
+        r = requests.get('http://api.alquran.cloud/v1/ayah/{}/en.ahmedraza'.format(ayahnumber))
+        output = r.json()
 
-    text = output['data']['text']
-    print(text)
+        text = output['data']['text']
+        print(text)
+    except:
+        print("Please enter a number between 1 and 6,236, as there are only 6,236 ayahs in the Quran")
 
 @cli.command('list-surahs')
 def list_surahs():
@@ -50,14 +57,18 @@ def list_surahs():
 @click.option('--word', help='Word you wish to search for', type=str, required=True)
 def search_quran(word):
     "Search all occurences for a word in the Quran"
-    r = requests.get('http://api.alquran.cloud/v1/search/{}/all/en.ahmedraza'.format(word))
-    output = r.json()
+    try:
+        r = requests.get('http://api.alquran.cloud/v1/search/{}/all/en.ahmedraza'.format(word))
+        output = r.json()
 
-    for i in output['data']['matches']:
-        t = PrettyTable(['Surah (English Name)', 'Surah (Arabic Name)', 'Text'])
-        t.add_row([i['surah']['englishName'], i['surah']['englishNameTranslation'], i['text']])
-        print(t)
+        for i in output['data']['matches']:
+            t = PrettyTable(['Surah (English Name)', 'Surah (Arabic Name)', 'Text'])
+            t.add_row([i['surah']['englishName'], i['surah']['englishNameTranslation'], i['text']])
+            print(t)
+    except:
+        print("That is not a valid option.  Please enter a valid word")
 
-    
+
+
 if __name__ == '__main__':
     cli()    
